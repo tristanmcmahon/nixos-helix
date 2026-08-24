d6d708991143bc0c1336662d112092ad0ed8e22e
tristanmcmahon <tristan.mcmahon@gmail.com>
2026-08-24 16:44:51 +1200

    Format disabled emulation test

--- tests/emulation-disabled.nix
+++ tests/emulation-disabled.nix
@@ -11,7 +11,8 @@ let
 in
 assert !config.helix.emulation.enable;
 assert !(builtins.any (mount: mount.where == "/mnt/infernalnexus/roms") config.systemd.mounts);
-assert !(builtins.any (automount: automount.where == "/mnt/infernalnexus/roms") config.systemd.automounts);
+assert
+  !(builtins.any (automount: automount.where == "/mnt/infernalnexus/roms") config.systemd.automounts);
 assert !(builtins.elem "helix-emulation-prepare" packageNames);
 assert !(builtins.hasAttr "helix-emulation-prepare" config.systemd.user.services);
 true
