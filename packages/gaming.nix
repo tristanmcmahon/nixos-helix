e6a8a943b02666df9cd552948bfca31f96fa7909
tristanmcmahon <tristan.mcmahon@gmail.com>
2026-08-29 15:29:51 +1200

    Install Doom source ports and launcher

--- packages/gaming.nix
+++ packages/gaming.nix
@@ -1,8 +1,29 @@
 { pkgs, ... }:
 
+let
+  doomSetup = pkgs.writeShellApplication {
+    name = "helix-doom-setup";
+    runtimeInputs = with pkgs; [
+      coreutils
+      curl
+      findutils
+      p7zip
+      unzip
+      util-linux
+    ];
+    text = builtins.readFile ../scripts/helix-doom-setup.sh;
+  };
+in
+
 {
   environment.systemPackages = with pkgs; [
     adwsteamgtk
+    doomrunner
+    doomSetup
+    gzdoom
     mangohud
+    uzdoom
   ];
+
+  environment.sessionVariables.DOOMWADDIR = "/mnt/games_nvme/doom/iwads";
 }
