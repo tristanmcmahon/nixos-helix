652d12da02b4b6e4f9668582ed6907a515151d5c
tristanmcmahon <tristan.mcmahon@gmail.com>
2026-09-21 22:22:27 +1200

    Keep strict invariants evaluation-only

--- tests/invariants/emulation.nix
+++ tests/invariants/emulation.nix
@@ -17,10 +17,4 @@ assert
   == "/mnt/games_nvme";
 assert lib.hasInfix "/mnt/games_nvme/emulation"
   config.systemd.services."helix-emulation-storage".script;
-assert builtins.elem "/home/tristan/Projects/nixos-helix"
-  config.systemd.user.services."openclaw-gateway".serviceConfig.BindPaths;
-assert builtins.elem "/mnt/games_nvme/emulation"
-  config.systemd.user.services."openclaw-gateway".serviceConfig.BindPaths;
-assert builtins.elem "-/mnt/infernalnexus"
-  config.systemd.user.services."openclaw-gateway".serviceConfig.ReadOnlyPaths;
 true
