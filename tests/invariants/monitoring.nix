eab69994053f9436674eed77308ed06b5af644f3
tristanmcmahon <tristan.mcmahon@gmail.com>
2026-09-26 23:38:01 +1200

    Use runtime symlink for Helix Netdata streaming (#77)
    * Use runtime symlink for Netdata stream config
    
    * Assert Netdata runtime stream symlink

--- tests/invariants/monitoring.nix
+++ tests/invariants/monitoring.nix
@@ -18,7 +18,7 @@ assert config.services.prometheus.exporters.smartctl.enable;
 assert config.services.prometheus.exporters.smartctl.listenAddress == "127.0.0.1";
 assert config.services.prometheus.exporters.smartctl.maxInterval == "2m";
 assert config.services.netdata.enable;
-assert builtins.hasAttr "stream.conf" config.services.netdata.configDir;
+assert config.services.netdata.configDir."stream.conf" == "/run/netdata-stream.conf";
 assert !config.services.netdata.enableAnalyticsReporting;
 assert config.services.netdata.package.withNdsudo;
 assert config.services.netdata.config.global.hostname == "helix";
@@ -33,8 +33,6 @@ assert builtins.hasAttr "go.d/smartctl.conf" config.services.netdata.configDir;
 assert builtins.hasAttr "go.d/systemdunits.conf" config.services.netdata.configDir;
 assert builtins.elem "netdata-stream-config.service" config.systemd.services.netdata.requires;
 assert builtins.elem "netdata-stream-config.service" config.systemd.services.netdata.after;
-assert builtins.elem "/run/netdata-stream.conf:/etc/netdata/conf.d/stream.conf"
-  config.systemd.services.netdata.serviceConfig.BindReadOnlyPaths;
 assert !(builtins.elem 19999 config.networking.firewall.allowedTCPPorts);
 assert config.services.grafana.enable;
 assert config.services.grafana.settings.server.http_addr == "127.0.0.1";
